95eb495991850c3be2a10e1d2b124f01b3146d12
TincoNomad <rtincopa@proton.me>
2024-01-13 18:01:22 -0500

    🚀release:smartcontract deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_TESNET_URL,
+      accounts: [process.env.ADMIN_ACCOUNT_PRIVATE_KEY /*, process.env.OTHER_ACCOUNT_PRIVATE_KEY*/ ],
+      timeout: 0,
+      gas: "auto",
+      gasPrice: "auto",
+    },
+  }
 };
